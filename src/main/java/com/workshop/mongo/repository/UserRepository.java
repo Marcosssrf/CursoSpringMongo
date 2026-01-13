b479d4d3fa3bb5635435976ee8c6d824fceccc23
Marcosssrf <marcosssrf@hotmail.com>
2026-01-13 12:51:32 -0300

    Correção de erro + conexão com mongodb com repository e service

---
+++ src/main/java/com/workshop/mongo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.workshop.mongo.repository;
+
+import com.workshop.mongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
